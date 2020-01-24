1998abf87290b77e5e6e15aa95bd4f6d0321c2db
vagif4799 <quliyev_vaqif@icloud.com>
2020-01-24 10:46:34 +0400

    -- spring security added

---
+++ src/main/java/Spring_Security_Demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,9 @@
+package Spring_Security_Demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+}
